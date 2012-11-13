a808a135eab09d1718223c18a1d2484a8833aa84
Michael <michael@michael-Studio-XPS-1640.(none)>
2012-11-13 03:35:50 -0600

    trying to fix settings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require admin_tools
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
 
 
-$(function() {
-	$('.edit_user input[type=submit]').remove();
-	$('.checking').live( 'change',function() {
-		$(this).parents('form:first').submit();
-	});
-});
+
